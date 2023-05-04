5dbb0ed9925c03276f2be768d0cfb6f14e412f70
kimchanho97 <nh0903@pusan.ac.kr>
2023-05-05 03:59:45 +0900

    docs: Boj2167-PrefixSum

--- PrefixSum/boj2167.py
+++ PrefixSum/boj2167.py
@@ -13,9 +13,6 @@ for i in range(1, n+1):
         # dp[i][j] = (0, 0)과 (i, j)을 대각선으로 가지는 사각형의 넓이
         dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1]
 
-for i in dp:
-    print(i)
-    
 k = int(sys.stdin.readline())
 for i in range(k):
     a, b, c, d = map(int, sys.stdin.readline().split())
